8063a780473fffeec8a5cb31cf2ce7a2a70c47c9
Marc-André Giroux <mgiroux0@gmail.com>
2016-05-21 14:39:12 -0400

    more problems

--- ninety_nine_problems.ml
+++ ninety_nine_problems.ml
@@ -130,3 +130,59 @@ let () = assert(
   encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] =
   [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
 )
+
+(* 11. Duplicate the elements of a list. (easy) *)
+
+let duplicate l =
+  let rec aux acc = function
+    | [] -> []
+    | [head] -> head :: head :: acc
+    | head :: tail -> aux (head :: head :: acc) tail in
+  reverse (aux [] l)
+
+let () = assert(duplicate ["a";"b";"c";"c";"d";] = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d";])
+
+
+(* 12. Replicate the elements of a list a given number of times. (medium) *)
+
+let replicate l n =
+  let rec prepend n acc x =
+    if n = 0 then acc else prepend (n-1) (x :: acc) x in
+  let rec aux acc = function
+    | [] -> acc
+    | head :: tail -> aux (prepend n acc head) tail in
+  aux [] (List.rev l);;
+
+let () = assert(replicate ["a";"b";"c";] 3 = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"])
+
+(* 13. Drop every N'th element from a list. (medium) *)
+
+let drop l n =
+  let rec aux acc count = function
+    | [] -> acc
+    | head :: tail -> if count = 0
+      then aux acc (n-1) tail
+      else aux (head :: acc) (count-1) tail in
+  List.rev (aux [] (n-1) l)
+
+let () = assert(
+  drop ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3 =
+  ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
+)
+
+(* 14. Split a list into two parts; the length of the first part is given. (easy) *)
+
+let split l n =
+  let rec aux i acc = function
+    | [] -> List.rev acc, []
+    | head :: tail as l -> if i = 0
+      then List.rev acc, l
+      else aux (i-1) (head :: acc) tail in
+    aux n [] l;;
+
+let () = assert(
+  split ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3 =
+  (["a"; "b"; "c"], ["d"; "e"; "f"; "g"; "h"; "i"; "j"])
+)
+
+(* 15. Extract a slice from a list. (medium) *)
